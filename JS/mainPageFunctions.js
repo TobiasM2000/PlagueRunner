393710fab23f2c0b77acd3cf501e1a0ad88e7423
Tobias Meißner <Tobias.Meissner@edu.fhdw.de>
2021-08-08 15:53:13 +0200

    MainMenuAnimation
    #18

--- JS/mainPageFunctions.js
+++ JS/mainPageFunctions.js
@@ -1,5 +1,23 @@
 function openNewPage(showpage, hidepage){
     document.getElementById(showpage).style.display='block';
     document.getElementById(hidepage).style.display='none';
-    
 }
+
+//Mainpage animation
+
+function copyImageToCanvas(){
+    var image = document.getElementById("ufo");
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    var frame = 0;
+    frame += 0,2;
+
+    var playercenter = image.clientWidth;
+
+    ctx.drawImage(image,100,100);
+}
+
+setTimeout(() => {
+   copyImageToCanvas(); 
+}, 300);
